bd6ce0127aa68695b1e810feb6c813acf603c4e2
League Student <league@iMac-28.attlocal.net>
2019-05-16 18:26:15 -0700

    Done

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,9 +1,13 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class LambdaButtons {
 	private JFrame window = new JFrame();
@@ -18,6 +22,22 @@ public class LambdaButtons {
 		window.add(randNumber);
 		window.add(tellAJoke);
 		
+		addNumbers.addActionListener((e)->{
+			String num = JOptionPane.showInputDialog("Enter a number:");
+			String num2 = JOptionPane.showInputDialog("Enter a number:");
+			int n = Integer.parseInt(num);
+			int n2 = Integer.parseInt(num2);
+			JOptionPane.showMessageDialog(null, num + " + " + num2 + " is " + (n + n2));
+		});
+		randNumber.addActionListener((e)->{
+			Random random = new Random();
+			int num = random.nextInt(1000000000);
+			JOptionPane.showMessageDialog(null, num);
+		});
+		tellAJoke.addActionListener((e)->{
+			JOptionPane.showMessageDialog(null, "Button out of order!");
+		});
+		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
 		
@@ -28,6 +48,12 @@ public class LambdaButtons {
 	public static void main(String[] args) {
 		new LambdaButtons();
 	}
+
+//	@Override
+//	public void actionPerformed(ActionEvent e) {
+//		// TODO Auto-generated method stub
+//		
+//	}
 	
 	
 }
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -1,5 +1,7 @@
 package _01_Lambda_Methods;
 
+import java.util.Random;
+
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
@@ -13,13 +15,47 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		printCustomMessage((s)->{
+			for (int i = 9; i > 0; i--) {
+				System.out.print(s.substring(i - 1, i));
+			}
+			System.out.println();
+		}, "backwards");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)->{
+			Random random = new Random();
+			for (int i = 0; i < 7; i++) {
+				if (random.nextInt(2) == 0) {
+					s = s.toLowerCase();
+					System.out.print(s.substring(i, i + 1));
+				} else {
+					s = s.toUpperCase();
+					System.out.print(s.substring(i, i + 1));
+				}
+			}
+			System.out.println();
+		}, "mixture");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((s)->{
+			for (int i = 0; i < 11; i++) {
+				if (i % 2 == 0) {
+					System.out.print(s.substring(i/2, i/2 + 1));
+				} else {
+					System.out.print(".");
+				}
+			}
+			System.out.println();
+		}, "glitch");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((s)->{
+			for (int i = 0; i < 9; i++) {
+				if (s.substring(i, i + 1 ).equals("a") || s.substring(i, i + 1 ).equals("e") || s.substring(i, i + 1 ).equals("i") || s.substring(i, i + 1 ).equals("o") || s.substring(i, i + 1 ).equals("u")) {
+					
+				} else {
+					System.out.print(s.substring(i, i + 1));
+				}
+			}
+		}, "novowels");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
